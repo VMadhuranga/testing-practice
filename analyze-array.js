8d3c5e42ccd4364e80316c53bbdc77573a7681bd
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-09-28 14:45:18 +0530

    Update analyze-array.js
    Update analyzeArray function to pass failed test cases

--- analyze-array.js
+++ analyze-array.js
@@ -1,6 +1,10 @@
 export default function analyzeArray(array) {
   const analyze = {};
 
+  if (!array) {
+    throw new Error("Empty value");
+  }
+
   analyze.average = array.reduce((pre, cur) => pre + cur) / array.length;
   analyze.min = Math.min(...array);
   analyze.max = Math.max(...array);
